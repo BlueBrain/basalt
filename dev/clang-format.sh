c01cc6babdfd3aa25d25a009c7f899845ae2b972
Tristan Carel <tristan.carel@gmail.com>
2018-11-26 18:31:16 +0100

    Update dev/ tools
    * cmake-format.sh: new script to format CMake scripts/
    * clang-format.sh: fix find command to gather source files
    * clang-tidy.sh: exit status != 0 if error count > 0
    * check.sh: new script to run the 3 scripts above at once

--- dev/clang-format.sh
+++ dev/clang-format.sh
@@ -3,10 +3,11 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 if ! ${CLANG_FORMAT:-clang-format} --version >/dev/null 2>&1 ; then
-    echo "clang-format is required. Override name with CLANG_FORMAT env var"
+    echo "clang-format is required. Override name with CLANG_FORMAT env var" >&2
     exit 1
-fi	
+fi
 
 find $SCRIPT_DIR/../include $SCRIPT_DIR/../src/basalt $SCRIPT_DIR/../test \
- -name "\*.hh" -o -name "\*.hxx" -name "\*.cc" \
- -exec ${CLANG_FORMAT:-clang-format} -i \{\} \;
+  -type f \
+  \( -name "*.hpp" -o -name "*.hxx" -o -name "*.cpp" \) \
+  -exec ${CLANG_FORMAT:-clang-format} -i \{\} \;
