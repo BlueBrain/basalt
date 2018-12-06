923f60ce73b07705ccc657b2d11f68d81d05323c
Tristan Carel <tristan.carel@gmail.com>
2018-12-06 17:40:24 +0100

    circuit payload type now have constructors to initialize fields

--- src/basalt/circuit_payloads.cpp
+++ src/basalt/circuit_payloads.cpp
@@ -12,14 +12,18 @@ namespace circuit {
 
 // neuron_t
 
+neuron_t::neuron_t(uint32_t gid, int_vector_t& astro_idx, int_vector_t& syn_idx)
+    : gid(gid), astro_idx(std::move(astro_idx)), syn_idx(std::move(syn_idx)) {}
+
 std::unique_ptr<neuron_t>
 neuron_t::create(uint32_t gid, pybind11::array_t<uint32_t>& astro_idx,
                  pybind11::array_t<uint32_t>& syn_idx) {
-    std::unique_ptr<neuron_t> eax(new neuron_t);
-    eax->gid = gid;
-    fill_vector(astro_idx, eax->astro_idx);
-    fill_vector(syn_idx, eax->syn_idx);
-    return eax;
+    int_vector_t astro_idx_vector;
+    int_vector_t syn_idx_vector;
+    fill_vector(astro_idx, astro_idx_vector);
+    fill_vector(syn_idx, syn_idx_vector);
+    return std::unique_ptr<neuron_t>(
+        new neuron_t(gid, astro_idx_vector, syn_idx_vector));
 }
 
 pybind11::array_t<char> neuron_t::serialize() const {
@@ -40,24 +44,27 @@ void neuron_t::deserialize(pybind11::array_t<char>& data) {
 
 // synapse_t
 
+synapse_t::synapse_t(uint32_t pre_gid, uint32_t post_gid, uint32_t nrn_idx,
+                     int_vector_t& astro_idx, bool is_excitatory,
+                     const float_point_t& pre, const float_point_t& post,
+                     const std::string& mesh_filename,
+                     const std::string& skeleton_filename, float psd_area)
+    : pre_gid(pre_gid), post_gid(post_gid), nrn_idx(nrn_idx),
+      astro_idx(std::move(astro_idx)), is_excitatory(is_excitatory),
+      pre(std::move(pre)), post(std::move(post)), mesh_filename(mesh_filename),
+      skeleton_filename(skeleton_filename), psd_area(psd_area) {}
+
 std::unique_ptr<synapse_t>
 synapse_t::create(uint32_t pre_gid, uint32_t post_gid, uint32_t nrn_idx,
                   pybind11::array_t<uint32_t>& astro_idx, bool is_excitatory,
                   const float_point_t& pre, const float_point_t& post,
                   const std::string& mesh_filename,
                   const std::string& skeleton_filename, float psd_area) {
-    std::unique_ptr<synapse_t> eax(new synapse_t);
-    eax->pre_gid = pre_gid;
-    eax->post_gid = post_gid;
-    eax->nrn_idx = nrn_idx;
-    fill_vector(astro_idx, eax->astro_idx);
-    eax->is_excitatory = is_excitatory;
-    eax->pre = pre;
-    eax->post = post;
-    eax->mesh_filename = mesh_filename;
-    eax->skeleton_filename = skeleton_filename;
-    eax->psd_area = psd_area;
-    return eax;
+    int_vector_t astro_idx_vector;
+    fill_vector(astro_idx, astro_idx_vector);
+    return std::unique_ptr<synapse_t>(new synapse_t(
+        pre_gid, post_gid, nrn_idx, astro_idx_vector, is_excitatory, pre, post,
+        mesh_filename, skeleton_filename, psd_area));
 }
 
 pybind11::array_t<char> synapse_t::serialize() const {
@@ -80,6 +87,19 @@ void synapse_t::deserialize(pybind11::array_t<char>& data) {
 
 // astrocyte_t
 
+astrocyte_t::astrocyte_t(uint32_t astrocyte_id, uint32_t microdomain_id,
+                         float soma_center_x, float soma_center_y,
+                         float soma_center_z, float soma_radius,
+                         const std::string& name, const std::string& mtype,
+                         const std::string& morphology_filename,
+                         int_vector_t& synapses_idx, int_vector_t& neurons_idx)
+    : astrocyte_id(astrocyte_id), microdomain_id(microdomain_id),
+      soma_center_x(soma_center_x), soma_center_y(soma_center_y),
+      soma_center_z(soma_center_z), soma_radius(soma_radius), name(name),
+      mtype(mtype), morphology_filename(morphology_filename),
+      synapses_idx(std::move(synapses_idx)),
+      neurons_idx(std::move(neurons_idx)) {}
+
 std::unique_ptr<astrocyte_t>
 astrocyte_t::create(uint32_t astrocyte_id, uint32_t microdomain_id,
                     float soma_center_x, float soma_center_y,
@@ -88,19 +108,14 @@ astrocyte_t::create(uint32_t astrocyte_id, uint32_t microdomain_id,
                     const std::string& morphology_filename,
                     pybind11::array_t<uint32_t>& synapses_idx,
                     pybind11::array_t<uint32_t>& neurons_idx) {
-    std::unique_ptr<astrocyte_t> eax(new astrocyte_t);
-    eax->astrocyte_id = astrocyte_id;
-    eax->microdomain_id = microdomain_id;
-    eax->soma_center_x = soma_center_x;
-    eax->soma_center_y = soma_center_y;
-    eax->soma_center_z = soma_center_z;
-    eax->soma_radius = soma_radius;
-    eax->name = name;
-    eax->mtype = mtype;
-    eax->morphology_filename = morphology_filename;
-    fill_vector(synapses_idx, eax->synapses_idx);
-    fill_vector(neurons_idx, eax->neurons_idx);
-    return eax;
+    int_vector_t synapses_idx_vector;
+    int_vector_t neurons_idx_vector;
+    fill_vector(synapses_idx, synapses_idx_vector);
+    fill_vector(neurons_idx, neurons_idx_vector);
+    return std::unique_ptr<astrocyte_t>(new astrocyte_t(
+        astrocyte_id, microdomain_id, soma_center_x, soma_center_y,
+        soma_center_z, soma_radius, name, mtype, morphology_filename,
+        synapses_idx_vector, neurons_idx_vector));
 }
 
 pybind11::array astrocyte_t::serialize() const {
@@ -124,6 +139,22 @@ void astrocyte_t::deserialize(pybind11::array_t<char>& data) {
 
 // microdomain_t
 
+microdomain_t::microdomain_t(uint32_t microdomain_id, uint32_t astrocyte_id,
+                             int_vector_t& neighbors,
+                             vertex_coordinates_t& vertex_coordinates,
+                             triangles_t& triangles,
+                             const float_point_t& centroid, double area,
+                             double volume, const std::string& mesh_filename,
+                             int_vector_t& neurons_idx,
+                             int_vector_t& synapses_idx)
+    : microdomain_id(microdomain_id), astrocyte_id(astrocyte_id),
+      neighbors(std::move(neighbors)),
+      vertex_coordinates(std::move(vertex_coordinates)),
+      triangles(std::move(triangles)), centroid(centroid), area(area),
+      volume(volume), mesh_filename(mesh_filename),
+      neurons_idx(std::move(neurons_idx)),
+      synapses_idx(std::move(synapses_idx)) {}
+
 std::unique_ptr<microdomain_t>
 microdomain_t::create(uint32_t microdomain_id, uint32_t astrocyte_id,
                       pybind11::array_t<uint32_t>& neighbors,
@@ -133,20 +164,20 @@ microdomain_t::create(uint32_t microdomain_id, uint32_t astrocyte_id,
                       const std::string& mesh_filename,
                       pybind11::array_t<uint32_t>& neurons_idx,
                       pybind11::array_t<uint32_t>& synapses_idx) {
-    std::unique_ptr<microdomain_t> eax(new microdomain_t);
-
-    eax->microdomain_id = microdomain_id;
-    eax->astrocyte_id = astrocyte_id;
-    fill_vector(neighbors, eax->neighbors);
-    fill_vector(vertex_coordinates, eax->vertex_coordinates);
-    fill_vector(triangles, eax->triangles);
-    eax->centroid = centroid;
-    eax->area = area;
-    eax->volume = volume;
-    eax->mesh_filename = mesh_filename;
-    fill_vector(neurons_idx, eax->neurons_idx);
-    fill_vector(synapses_idx, eax->synapses_idx);
-    return eax;
+    int_vector_t neighbors_vector;
+    vertex_coordinates_t vertex_coordinates_vector;
+    triangles_t triangles_vector;
+    int_vector_t neurons_idx_vector;
+    int_vector_t synapses_idx_vector;
+    fill_vector(neighbors, neighbors_vector);
+    fill_vector(vertex_coordinates, vertex_coordinates_vector);
+    fill_vector(triangles, triangles_vector);
+    fill_vector(neurons_idx, neurons_idx_vector);
+    fill_vector(synapses_idx, synapses_idx_vector);
+    return std::unique_ptr<microdomain_t>(new microdomain_t(
+        microdomain_id, astrocyte_id, neighbors_vector,
+        vertex_coordinates_vector, triangles_vector, centroid, area, volume,
+        mesh_filename, neurons_idx_vector, synapses_idx_vector));
 }
 
 pybind11::array microdomain_t::serialize() const {
@@ -178,24 +209,19 @@ void microdomain_t::deserialize(pybind11::array_t<char>& data) {
 
 // segment_t
 
+segment_t::segment_t(uint32_t section_id, uint32_t segment_id, uint8_t type,
+                     float x1, float y1, float z1, float r1, float x2, float y2,
+                     float z2, float r2)
+    : section_id(section_id), segment_id(segment_id), type(type), x1(x1),
+      y1(y1), z1(z1), r1(r1), x2(x2), y2(y2), z2(z2), r2(r2) {}
+
 std::unique_ptr<segment_t> segment_t::create(uint32_t section_id,
                                              uint32_t segment_id, uint8_t type,
                                              float x1, float y1, float z1,
                                              float r1, float x2, float y2,
                                              float z2, float r2) {
-    std::unique_ptr<segment_t> eax(new segment_t);
-    eax->section_id = section_id;
-    eax->segment_id = segment_id;
-    eax->type = type;
-    eax->x1 = x1;
-    eax->y1 = y1;
-    eax->z1 = z1;
-    eax->r1 = r1;
-    eax->x2 = x2;
-    eax->y2 = y2;
-    eax->z2 = z2;
-    eax->r2 = r2;
-    return eax;
+    return std::unique_ptr<segment_t>(new segment_t(
+        section_id, segment_id, type, x1, y1, z1, r1, x2, y2, z2, r2));
 }
 
 pybind11::array segment_t::serialize() const {
@@ -215,14 +241,16 @@ void segment_t::deserialize(pybind11::array_t<char>& data) {
 
 // edge_astrocyte_segment_t
 
+edge_astrocyte_segment_t::edge_astrocyte_segment_t(
+    const float_point_t& astrocyte, const float_point_t& vasculature)
+    : astrocyte(astrocyte), vasculature(vasculature) {}
+
 std::unique_ptr<edge_astrocyte_segment_t>
 edge_astrocyte_segment_t::create(const float_point_t& astrocyte,
                                  const float_point_t& vasculature) {
 
-    std::unique_ptr<edge_astrocyte_segment_t> eax(new edge_astrocyte_segment_t);
-    eax->astrocyte = astrocyte;
-    eax->vasculature = vasculature;
-    return eax;
+    return std::unique_ptr<edge_astrocyte_segment_t>(
+        new edge_astrocyte_segment_t(astrocyte, vasculature));
 }
 
 pybind11::array edge_astrocyte_segment_t::serialize() const {
