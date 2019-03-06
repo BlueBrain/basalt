2a0dbd41f4238b7d28e7341034c8a8178ea1d5eb
Tristan Carel <tristan.carel@gmail.com>
2019-03-06 14:54:01 +0100

    improve variable namings to avoid confusion in NGV import
    Take @ohm314 remarks into account.

--- src/basalt/py_basalt.cpp
+++ src/basalt/py_basalt.cpp
@@ -387,24 +387,24 @@ PYBIND11_MODULE(_basalt, m) {  // NOLINT
                          throw std::runtime_error("Number of nodes and payloads differ");
                      }
                  }
-                 std::vector<const char*> node_payloads_data;
-                 node_payloads_data.reserve(payloads.size());
-                 std::vector<std::size_t> node_payloads_sizes;
-                 node_payloads_sizes.reserve(payloads.size());
-                 for (py::handle handle: payloads) {
-                     py::array_t<char> node_payload = py::cast<py::array_t<char>>(handle);
-                     node_payloads_data.push_back(node_payload.data());
-                     node_payloads_sizes.push_back(static_cast<std::size_t>(node_payload.size()));
+                 std::vector<const char*> payloads_data;
+                 std::vector<std::size_t> payloads_sizes;
+                 payloads_data.reserve(payloads.size());
+                 payloads_sizes.reserve(payloads.size());
+                 for (py::handle list_item: payloads) {
+                     py::array_t<char> payload = py::cast<py::array_t<char>>(list_item);
+                     payloads_data.push_back(payload.data());
+                     payloads_sizes.push_back(static_cast<std::size_t>(payload.size()));
                  }
-                 if (node_payloads_data.empty()) {
+                 if (payloads_data.empty()) {
                      instance
                          .insert(types.data(), nodes.data(), nullptr, nullptr,
                                  static_cast<std::size_t>(nodes.size()), commit)
                          .raise_on_error();
                  } else {
                      instance
-                         .insert(types.data(), nodes.data(), node_payloads_data.data(),
-                                 node_payloads_sizes.data(), static_cast<std::size_t>(nodes.size()),
+                         .insert(types.data(), nodes.data(), payloads_data.data(),
+                                 payloads_sizes.data(), static_cast<std::size_t>(nodes.size()),
                                  commit)
                          .raise_on_error();
                  }
