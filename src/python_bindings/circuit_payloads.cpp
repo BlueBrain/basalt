eee7dce48ac086a2c931a5c1b7c309ecde7c2c7c
Tristan Carel <tristan.carel@gmail.com>
2019-08-14 15:00:38 +0200

    add copyright in source headers + script to do it (#96)

--- src/python_bindings/circuit_payloads.cpp
+++ src/python_bindings/circuit_payloads.cpp
@@ -1,3 +1,9 @@
+/*************************************************************************
+ * Copyright (C) 2019 Blue Brain Project
+ *
+ * This file is part of Basalt distributed under the terms of the GNU
+ * Lesser General Public License. See top-level LICENSE file for details.
+ *************************************************************************/
 /**
  * \file src/basalt/circuit_payloads.cpp
  * Provides definition of classes used as payload for vertex
