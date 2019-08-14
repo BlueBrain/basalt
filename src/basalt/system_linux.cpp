b833d860639aca038bbad3f025ec4443d6aa778a
Tristan Carel <tristan.carel@gmail.com>
2019-08-14 14:00:43 +0200

    add copyright in source headers + script to do it

--- src/basalt/system_linux.cpp
+++ src/basalt/system_linux.cpp
@@ -1,3 +1,9 @@
+/*************************************************************************
+ * Copyright (C) 2019 Blue Brain Project
+ *
+ * This file is part of Basalt distributed under the terms of the GNU
+ * Lesser General Public License. See top-level LICENSE file for details.
+ *************************************************************************/
 #include <cerrno>
 #include <cstring>
 #include <stdexcept>
