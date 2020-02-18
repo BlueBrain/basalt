54167c0224c068071befbe7cc1781935f7f64b36
Tristan Carel <tristan.carel@gmail.com>
2020-02-18 13:10:30 +0100

    Bump third-parties
    * catch2: 2.9.2 -> 2.11.1
    * cereal: 1.2.2 -> 1.3.0
    * fmt: 6.0.0 -> 6.1.2
    * gsl-lite: 0.34 -> 0.36
    * json: 3.7.0 -> 3.7.3
    * pybind11: 2.3.0 -> 2.4.3
    * spdlog: 1.3.1 -> 1.5.0

--- src/basalt/graph_impl.cpp
+++ src/basalt/graph_impl.cpp
@@ -16,9 +16,11 @@
 #include <rocksdb/slice_transform.h>
 #include <rocksdb/statistics.h>
 #include <rocksdb/table.h>
-#include <spdlog/fmt/ostr.h>
+// clang-format off
+#include <spdlog/spdlog.h>
 #include <spdlog/sinks/rotating_file_sink.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
+// clang-format on
 
 namespace basalt {
 
